c331850e916606e8e3eb65efb98d03952f3981dc
Ryan.Mills <146854317+RyanJosephMills@users.noreply.github.com>
2026-04-28 20:01:13 +0100

    Fixed My Hiding Spot

---
+++ Assets/Scripts/Return to camera.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using static UnityEditor.Experimental.GraphView.GraphView;
+
+public class Returntocamera : MonoBehaviour
+{
+    public Camera Camera1;
+    public Camera Camera2;
+    PlayerMovement playerMovement;
+    public GameObject playerg;
+    public GameObject hideText;
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        playerMovement = FindObjectOfType<PlayerMovement>();
+    }
+        void Start()
+    {
+        Camera1.gameObject.SetActive(false);
+        Camera2.gameObject.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Q))
+        {
+            playerg.SetActive(true);
+            // player.position = desternation.position;
+            Camera1.gameObject.SetActive(!Camera1.gameObject.activeSelf);
+            Camera2.gameObject.SetActive(!Camera2.gameObject.activeSelf);
+            hideText?.SetActive(false);
+        }
+    }
+}
--- Assets/Scripts/Teleporter.cs
+++ Assets/Scripts/Teleporter.cs
@@ -5,28 +5,60 @@ using UnityEngine;
 public class Teleporter : MonoBehaviour
 {
 
-    public Transform player, desternation;
+   // public Transform player, desternation;
     public GameObject playerg;
+    PlayerMovement playerMovement;
+    public bool inReach;
+    public GameObject hideText;
+    public Camera Camera1;
+    public Camera Camera2;
+    enemyAiPartrol EnemyCode;
+
+
+    private void Awake()
+    {
+        playerMovement = FindObjectOfType<PlayerMovement>();
+        EnemyCode = FindObjectOfType<enemyAiPartrol>();
+    }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player"))
+        if (other.gameObject.tag == "Reach")
         {
-            playerg.SetActive(false);
-            player.position = desternation.position;
-            playerg.SetActive(true);
+            inReach = true;
+            hideText.SetActive(true);
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.gameObject.tag == "Reach")
+        {
+            hideText.SetActive(false);
+            inReach = false;
         }
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        inReach = false;
+        Camera1.gameObject.SetActive(true);
+        Camera2.gameObject.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (playerMovement.IsInteractPressed && inReach)
+        {
+            playerg.SetActive(false);
+           // player.position = desternation.position;
+            inReach = false;
+            Camera1.gameObject.SetActive(!Camera1.gameObject.activeSelf);
+            Camera2.gameObject.SetActive(!Camera2.gameObject.activeSelf);
+            hideText.SetActive(false);
+            EnemyCode.playerInSight = false;
+            
+        }
     }
 }
